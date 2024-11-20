2e14ad26b29ec79b161af29ab924cd3210712b21
Caner Sürmelioğlu <canersurmelioglu@gmail.com>
2024-11-20 14:53:21 +0300

    24-09-30
    24-09-30 Tarihindeki çalışmam

---
+++ 24-09-30/24-09-30/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _24_09_30
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Int32 soru;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            soru = Convert.ToInt32(MessageBox.Show("PROGRAM KAPATILSIN MI?", "KAPAT",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2));
+
+            if (soru == 6)
+            {
+                Application.Exit();
+                //this.Close;
+            }
+
+
+
+
+        }
+
+        DialogResult cikis;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // BU YÖNTEM C# IN KENDİ METODUDUR
+            cikis = MessageBox.Show("PROGRAM KAPATILSIN MI?", "KAPAT",
+               MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2);
+
+            if (cikis == DialogResult.Yes)
+            {
+                Application.Exit();
+                //this.Close;
+            }
+
+        }
+
+        void bilgi(string adi, string soyadi)
+        {
+            textBox3.Text = "GELEN DEGERLER: " + adi + " " + soyadi;
+            label1.Text = "gelen degerler: " + adi + " " + soyadi;
+            MessageBox.Show("gelen degerler: " + adi + " " + soyadi);
+        }
+
+        void temizle()
+        {
+            textBox1.Text = "";
+            textBox1.Clear();
+            textBox1.Focus();
+            textBox3.Clear();
+            label1.Text = "";
+        }
+        string adi;
+        string soyadi;
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            if (textBox1.Text.Trim() == "")
+            {
+                MessageBox.Show("MUSTERININ ADINI YAZINIZ");
+                textBox1.Focus();
+                
+            }
+            else
+
+                if (textBox2.Text.Trim() == "")
+                {
+                MessageBox.Show("MUSTERININ SOYADINI YAZINIZ");
+                textBox2.Focus();
+                
+                }
+
+                else
+                {
+                    adi = textBox1.Text.ToUpper().Trim();
+                    soyadi = textBox2.Text.ToUpper().Trim();
+                    bilgi(adi, soyadi);
+                }
+
+
+        }
+        
+        //fonksiyon oluşturma
+        double hesapla(double a, double b)
+        {
+            double toplam = a + b;
+            return toplam;
+        }
+        double a, b;
+        private void button4_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(textBox1.Text);
+            b = Convert.ToDouble(textBox1.Text);
+            textBox3.Text = "Toplamın sonucu : " + hesapla(a,b);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            Form2 siyah = new Form2();
+            siyah.ShowDialog();
+
+        }
+    }
+}
+
+        
+    
+
---
+++ 24-09-30/24-09-30/Form2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _24_09_30
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        Int32 faktoryel(Int32 sayi)
+        {
+            int fakt = 1;
+            int i;
+            for (i = 1; i <= sayi; i++)
+            {
+                fakt *= i;
+                
+            }
+            return fakt;
+        }
+        Int32 sayi;
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //SORU: Text nesnesine girilen bir sayının faktorielini hesaplayınız
+            //özellikler: 1: girilen sayı 1-9 arasında olmalıdır 
+        //2: girilen sayı negatif olmamalıdır negatif ise pozitif yapılmalıdır
+        //3: girilen sayı faktoriel isimli fonksiyona gönderilerek sonucu mesajla göstermelidir
+            sayi = Convert.ToInt32(textBox1.Text);
+
+            if (sayi < 0)
+            {
+                //sayi *= -1;
+                Math.Abs(sayi);
+            }
+            if (sayi < 1 || sayi > 9)
+            {
+                MessageBox.Show("1-9 arasında sayi giriniz..");
+            }
+            else
+            {
+            MessageBox.Show("sonucun degeri :"+faktoryel(sayi));
+
+            }
+        }
+    }
+}
